57c9c5f840737e430bfe4151d6faf1efbb6c6688
dosas <dosas@github.com>
2024-06-18 23:14:33 +0200

    Adapt config

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 # CONFIG
 #########################################
 
-GLOBAL_VOLUME_DB = -6
-AUDIO_DEVICE_ID = 0                     # change this number to use another soundcard
+GLOBAL_VOLUME_DB = -3
+AUDIO_DEVICE_ID = 1                     # change this number to use another soundcard
 SAMPLES_DIR = "/media/samples"          # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
 MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
 USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
 USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C
 USE_I2C_DISPLAY = True
-I2C_PORT = 0
+I2C_PORT = 1
 USE_SYSTEMLED = False                   # Flashing LED after successful boot, only works on RPi/Linux
 USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
 SERIALPORT_PORT = '/dev/ttyACM0'
